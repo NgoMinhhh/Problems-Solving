f5e956c2aac9426260c4c7e01709a7ec4ceb4d29
Minh Ngo <ngonhatminh9898@gmail.com>
2023-06-07 14:02:45 +0930

    add create_event()

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,9 +1,9 @@
 def main():
-    timetable = []
+    timetable : list[dict[str,str]] = []
 
     while True:
         print("==== Timetable Management ====")
-        # print("1. Create Event")
+        print("1. Create Event")
         # print("2. Update Event")
         # print("3. Delete Event")
         # print("4. Print Timetable")
@@ -14,10 +14,26 @@ def main():
         match choice:
             case '0':
                 return
+            case '1':
+                create_event(timetable)
             case _:
                 continue
+        print()
 
+def create_event(timetable: list[dict[str,str]]) -> None:
+    """Create valid event"""
+    # Ask title
+    if not(title := input("Title: ")):
+        print("Title can not be empty!")
+        return
+    
+    # Ask Loc
+    location = input("Location (optional): ")
 
+    # Ask day & time
+    day = input("Day (e.g. monday, tue): ")
+    start = input("Time start (e.g. 7am, 10:30pm): ")
+    end = input("Time end (e.g. 7pm, 10:30am): ")
 
 if __name__ == '__main__':
     main()
